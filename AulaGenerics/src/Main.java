6fb9de5415b0d120f9503fbfdaf0c8932e9368c7
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2022-09-28 15:47:30 -0300

    Pega o arquivo nomes

--- AulaGenerics/src/Main.java
+++ AulaGenerics/src/Main.java
@@ -1,10 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Stream;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
 	public static ListaTelefonica lista = new ListaTelefonica();
@@ -20,15 +20,26 @@ public class Main {
 	}
 	
 	public static void main(String args[]) {
+		
 		List<Contato> listaContatos = new ArrayList<Contato>();
-		
-			
-		
-		
-		Contato c1 = new Contato("Jão", 11231235);
+		//Trocar o filepath para o da máquina sendo usada;
+		String filePath = "C:\\\\Users\\\\12046333985\\\\eclipse-workspace\\\\ExCollections\\\\src\\\\nomes.txt";
+		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+			String line;
+			while ((line = br.readLine()) != null) {
+				int randomInt = ThreadLocalRandom.current().nextInt(0, 1000000000);
+				listaContatos.add(new Contato(line, randomInt));
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		lista.setContatos(listaContatos);
+		/* Contato c1 = new Contato("Jão", 11231235);
 		Contato c2 = new Contato("José", 198426187);
 		lista.adicionarContato(c1);
-		lista.adicionarContato(c2);
+		lista.adicionarContato(c2); */
 		exibirContatos();
 	}
 }
