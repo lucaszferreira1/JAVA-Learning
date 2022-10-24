dab8f99d097f69431bb2321317b10df13994ac7f
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2022-10-24 16:34:18 -0300

    Update Main.java

--- Lista3/L3E2/src/Main.java
+++ Lista3/L3E2/src/Main.java
@@ -1,17 +1,20 @@
 
 public class Main {
 	public static void main(String args[]) {
-		TrianguloEquilatero tri = new TrianguloEquilatero(3);
-		Losango los = new Losango(3, 4);
-		Circulo cir = new Circulo(5);
+		TrianguloEquilatero tri1 = new TrianguloEquilatero(3);
+		TrianguloEquilatero tri2 = new TrianguloEquilatero(8);
+		Losango los1 = new Losango(6, 2);
+		Losango los2 = new Losango(3, 4);
+		Circulo cir1 = new Circulo(5);
+		Circulo cir2 = new Circulo(10);
 		
-		System.out.println(tri.calculaArea());
-		System.out.println(tri.calculaPerimetro());
+		System.out.println(tri1.toString());
+		System.out.println(tri2.toString());
 		System.out.println();
-		System.out.println(los.calculaArea());
-		System.out.println(los.calculaPerimetro());
+		System.out.println(los1.toString());
+		System.out.println(los2.toString());
 		System.out.println();
-		System.out.println(cir.calculaArea());
-		System.out.println(cir.calculaPerimetro());
+		System.out.println(cir1.toString());
+		System.out.println(cir2.toString());
 	}
 }
