d2657218097ebfd18d6d5a24d57ebabfe217060c
lucaszferreira1 <48135696+lucaszferreira1@users.noreply.github.com>
2022-11-09 16:55:47 -0300

    Update MainWindow.java

--- AulaInterfaceGrafica/src/MainWindow.java
+++ AulaInterfaceGrafica/src/MainWindow.java
@@ -4,7 +4,7 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.FillLayout;
 
-
+import java.util.ArrayList;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.RowLayout;
@@ -61,6 +61,59 @@ public class MainWindow {
 		shell.setText("SWT Application");
 		shell.setLayout(null);
 		
+		List list = new List(shell, SWT.BORDER);
+		list.setBounds(281, 161, 160, 195);
+		
+		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setBounds(40, 35, 603, 45);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn clmnSort = new TableColumn(table, SWT.NONE);
+		clmnSort.setWidth(65);
+		clmnSort.setText("Sorteado");
+		
+		TableColumn clmnSum = new TableColumn(table, SWT.NONE);
+		clmnSum.setWidth(71);
+		clmnSum.setText("Somatorio");
+		
+		TableColumn clmnArith = new TableColumn(table, SWT.NONE);
+		clmnArith.setWidth(108);
+		clmnArith.setText("Média Aritmética");
+		
+		TableColumn clmnGeom = new TableColumn(table, SWT.NONE);
+		clmnGeom.setWidth(108);
+		clmnGeom.setText("Média Geométrica");
+		
+		TableColumn clmnVari = new TableColumn(table, SWT.NONE);
+		clmnVari.setWidth(65);
+		clmnVari.setText("Variância");
+		
+		TableColumn clmnDePa = new TableColumn(table, SWT.NONE);
+		clmnDePa.setWidth(86);
+		clmnDePa.setText("Desvio Padrão");
+		
+		TableColumn clmnAmp = new TableColumn(table, SWT.NONE);
+		clmnAmp.setWidth(85);
+		clmnAmp.setText("Amplitude");
+		
+		Text txtDigiteUmValor = new Text(shell, SWT.BORDER);
+		txtDigiteUmValor.setToolTipText("Digite um valor");
+		txtDigiteUmValor.setBounds(40, 181, 160, 21);
+		
+		Button btnAdicionar = new Button(shell, SWT.NONE);
+		btnAdicionar.setText("Adicionar");
+		btnAdicionar.setBounds(80, 226, 75, 25);
+		btnAdicionar.addListener(SWT.Selection, new Listener() {
+			public void handleEvent(Event e) {
+			switch(e.type) {	
+				case SWT.Selection:
+						int input = Integer.parseInt(txtDigiteUmValor.getText());
+						break;
+				}
+			}
+		});
+		
 		Button btnLimpar = new Button(shell, SWT.NONE);
 		btnLimpar.setBounds(80, 275, 75, 25);
 		btnLimpar.setText("Limpar");
@@ -69,80 +122,78 @@ public class MainWindow {
 			switch(e.type) {	
 				case SWT.Selection:
 						txtDigiteUmValor.setText("");
+						limpar(list);
 						break;
 				}
 			}
 		});
 		
-		txtDigiteUmValor = new Text(shell, SWT.BORDER);
-		txtDigiteUmValor.setToolTipText("Digite um valor");
-		txtDigiteUmValor.setBounds(40, 181, 160, 21);
-		
-		btnAdicionar = new Button(shell, SWT.NONE);
-		btnAdicionar.setText("Adicionar");
-		btnAdicionar.setBounds(80, 226, 75, 25);
-		btnAdicionar.addListener(SWT.Selection, new Listener() {
-			public void handleEvent(Event e) {
-			switch(e.type) {	
-				case SWT.Selection:
-						System.out.println(txtDigiteUmValor.getText());
-						break;
-				}
-			}
-		});
-		
-		List list = new List(shell, SWT.BORDER);
-		list.setBounds(281, 161, 160, 195);
-		
-		Button btnAdicionar_1 = new Button(shell, SWT.NONE);
-		btnAdicionar_1.setText("Fibonacci");
-		btnAdicionar_1.setBounds(529, 194, 75, 25);
-		
-		Button btnNewButton_1 = new Button(shell, SWT.NONE);
-		btnNewButton_1.setText("Primos");
-		btnNewButton_1.setBounds(529, 226, 75, 25);
-		
-		table = new Table(shell, SWT.BORDER | SWT.FULL_SELECTION);
-		table.setBounds(40, 35, 603, 45);
-		table.setHeaderVisible(true);
-		table.setLinesVisible(true);
-		
-		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn.setWidth(65);
-		tblclmnNewColumn.setText("Sorteado");
-		
-		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn_1.setWidth(71);
-		tblclmnNewColumn_1.setText("Somatorio");
-		
-		TableColumn tblclmnNewColumn_2 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn_2.setWidth(108);
-		tblclmnNewColumn_2.setText("Média Aritmética");
-		
-		TableColumn tblclmnNewColumn_3 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn_3.setWidth(108);
-		tblclmnNewColumn_3.setText("Média Geométrica");
-		
-		TableColumn tblclmnNewColumn_4 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn_4.setWidth(65);
-		tblclmnNewColumn_4.setText("Variância");
-		
-		TableColumn tblclmnNewColumn_5 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn_5.setWidth(86);
-		tblclmnNewColumn_5.setText("Desvio Padrão");
-		
-		TableColumn tblclmnNewColumn_6 = new TableColumn(table, SWT.NONE);
-		tblclmnNewColumn_6.setWidth(85);
-		tblclmnNewColumn_6.setText("Amplitude");
-		
-		Button btnAdicionar_1_1 = new Button(shell, SWT.NONE);
-		btnAdicionar_1_1.addSelectionListener(new SelectionAdapter() {
+		Button btnFibon = new Button(shell, SWT.NONE);
+		btnFibon.setText("Fibonacci");
+		btnFibon.setBounds(529, 194, 75, 25);
+		btnFibon.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent ev) {
+				int input = Integer.parseInt(txtDigiteUmValor.getText());
+				Fibonacci fib =  new Fibonacci();
+				fib.gerar(input);
+				java.util.List<Integer> sequencia = fib.getSequencia();
+				adicionaValor(sequencia, list);
 			}
 		});
-		btnAdicionar_1_1.setText("Fatorial");
-		btnAdicionar_1_1.setBounds(529, 257, 75, 25);
+		
+		Button btnPrime = new Button(shell, SWT.NONE);
+		btnPrime.setText("Primos");
+		btnPrime.setBounds(529, 226, 75, 25);
+		btnPrime.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent ev) {
+				int input = Integer.parseInt(txtDigiteUmValor.getText());
+				NumeroPrimos pri =  new NumeroPrimos();
+				pri.gerar(input);
+				java.util.List<Integer> sequencia = pri.getSequencia();
+				adicionaValor(sequencia, list);
+			}
+		});
+		
+		Button btnFat = new Button(shell, SWT.NONE);
+		btnFat.setText("Fatorial");
+		btnFat.setBounds(529, 257, 75, 25);
+		btnFat.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent ev) {
+				try {
+					int input = Integer.parseInt(txtDigiteUmValor.getText());
+					Fatoriais fat =  new Fatoriais();
+					fat.gerar(input);
+					java.util.List<Integer> sequencia = fat.getSequencia();
+					adicionaValor(sequencia, list);
+					generateParams(fat, table);
+				}catch(Exception e) {
+					System.err.println("Erro ao tentar gerar a lista!");
+				}
+				
+			}
+		});
 
+	}
+	
+	private void adicionaValor(java.util.List<Integer> values, List lista) {
+		lista.removeAll();
+		for (Integer i : values){
+			lista.add(i.toString());
+		}
+	}
+	
+	private void limpar(List lista) {
+		lista.removeAll();
+	}
+	
+	private void generateParams(Object o, Table table) {
+		try {
+			
+		}catch(Exception e) {
+			System.err.print("Erro ao tentar listar seus atributos");
+		}
 	}
 }
